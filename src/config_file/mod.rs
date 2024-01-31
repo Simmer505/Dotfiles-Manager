f12ecc0987b236079caa2753f17129284761c56b
Ethan Simmons <eesimmons9105@gmail.com>
2024-01-30 22:30:57 -0600

    A couple minor fixes

--- src/config_file/mod.rs
+++ src/config_file/mod.rs
@@ -16,12 +16,20 @@ impl ConfigFile {
     pub fn new(rel_git_location: PathBuf, sys_location: PathBuf) -> Result<Self, Box<dyn Error>> {
 
         let home_dir = PathBuf::from(env::var("HOME").expect("$HOME not set"));
-        let manager_dir = home_dir.join(".dotfiles/");
+        let manager_dir = home_dir.join(PathBuf::from(".dotfiles/"));
         
         let manager_path = manager_dir.join(rel_git_location);
         let system_path  = sys_location;
 
-        let is_dir = fs::metadata(&manager_path)?.is_dir(); 
+        let manager_path_data = fs::metadata(&manager_path);
+        let sys_path_data = fs::metadata(&system_path);
+
+        let is_dir = match (manager_path_data, sys_path_data) {
+            (Ok(manager_data), Ok(sys_data)) => manager_data.is_dir() && sys_data.is_dir(),
+            (Ok(manager_data), Err(_)) => manager_data.is_dir(),
+            (Err(_), Ok(sys_data)) => sys_data.is_dir(),
+            (Err(e1), Err(e2)) => panic!("Neither {} nor {} exists or is readable: {}, {}", manager_path.to_str().unwrap(), system_path.to_str().unwrap(), e1, e2),
+        };
 
         Ok(Self { manager_path, system_path, is_dir })
     }
